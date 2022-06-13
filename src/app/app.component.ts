d7ab7c1464877ed1ac4061b7a37661b89bac63f2
SSDFarhan <farhan@halimsol.com>
2022-06-13 12:53:03 +0500

    Add AppUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: { id: number; userName: string }[] | undefined;
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.http.get('http://localhost:5154/api/users').subscribe({
+      next: (response: any) => (this.users = response),
+      error: (error: any) => console.log(error),
+    });
+  }
 }
